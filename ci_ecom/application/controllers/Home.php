67db0894b8fa2d08c7d507a1182e04440f2c619b
SENG Sourng <sourng2009@gmail.com>
2018-05-26 11:11:21 +0700

    update

---
+++ ci_ecom/application/controllers/Home.php
@@ -0,0 +1,92 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Home extends CI_Controller {
+
+	public function index()
+	{
+		$this->load->view('index');	
+	}
+	
+	public function category()
+	{
+		$this->load->view('category');
+		
+	}
+		public function detail()
+	{
+		$this->load->view('detail');
+		
+	}
+		public function text()
+	{
+		$this->load->view('text');
+		
+	}
+		public function category_full()
+		{
+			$this->load->view('category_full');
+		}
+		public function category_right()
+		{
+			$this->load->view('category_right');
+		}
+			public function register()
+		{
+			$this->load->view('register');
+		}
+			public function contact()
+		{
+			$this->load->view('contact');
+		}
+		
+			public function customer_orders()
+		{
+			$this->load->view('customer_orders');
+		}
+			public function customer_wishlist()
+		{
+			$this->load->view('customer_wishlist'); 
+		}
+			public function customer_account()
+		{
+			$this->load->view('customer_account');
+		}
+			public function basket()
+		{
+			$this->load->view('basket');
+		}
+			public function faq()
+		{
+			$this->load->view('faq');
+		}
+			public function post()
+		{
+			$this->load->view('post');
+		}
+			public function blog()
+		{
+			$this->load->view('blog');
+		}
+			public function checkout1()
+		{
+			$this->load->view('checkout1');
+		}
+			public function checkout2()
+		{
+			$this->load->view('checkout2');
+		}
+			public function checkout3()
+		{
+			$this->load->view('checkout3');
+		}
+			public function checkout4()
+		{
+			$this->load->view('checkout4');
+		}
+
+
+
+
+
+}
---
+++ ci_ecom/application/views/inc/category_left.php
@@ -0,0 +1,53 @@
+ <div class="panel panel-default sidebar-menu">
+
+                        <div class="panel-heading">
+                            <h3 class="panel-title">Categories</h3>
+                        </div>
+
+                        <div class="panel-body">
+                            <ul class="nav nav-pills nav-stacked category-menu">
+                                <li>
+                                    <a href="<?php echo site_url(); ?>home/category">Men <span class="badge pull-right">42</span></a>
+                                    <ul>
+                                        <li><a href="<?php echo site_url(); ?>home/category">T-shirts</a>
+                                        </li>
+                                        <li><a href="<?php echo site_url(); ?>home/category">Shirts</a>
+                                        </li>
+                                        <li><a href="<?php echo site_url(); ?>home/category">Pants</a>
+                                        </li>
+                                        <li><a href="<?php echo site_url(); ?>home/category">Accessories</a>
+                                        </li>
+                                    </ul>
+                                </li>
+                                <li class="active">
+                                    <a href="<?php echo site_url(); ?>home/category">Ladies  <span class="badge pull-right">123</span></a>
+                                    <ul>
+                                        <li><a href="<?php echo site_url(); ?>home/category">T-shirts</a>
+                                        </li>
+                                        <li><a href="<?php echo site_url(); ?>home/category">Skirts</a>
+                                        </li>
+                                        <li><a href="<?php echo site_url(); ?>home/category">Pants</a>
+                                        </li>
+                                        <li><a href="<?php echo site_url(); ?>home/category">Accessories</a>
+                                        </li>
+                                    </ul>
+                                </li>
+                                <li>
+                                    <a href="<?php echo site_url(); ?>home/category">Kids  <span class="badge pull-right">11</span></a>
+                                    <ul>
+                                        <li><a href="<?php echo site_url(); ?>home/category">T-shirts</a>
+                                        </li>
+                                        <li><a href="<?php echo site_url(); ?>home/category">Skirts</a>
+                                        </li>
+                                        <li><a href="<?php echo site_url(); ?>home/category">Pants</a>
+                                        </li>
+                                        <li><a href="<?php echo site_url(); ?>home/category">Accessories</a>
+                                        </li>
+                                    </ul>
+                                </li>
+
+                            </ul>
+
+                        </div>
+                    </div>
+
