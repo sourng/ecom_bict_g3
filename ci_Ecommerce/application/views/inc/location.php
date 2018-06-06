493fcfa533c308d0f5b056f13339051880307f76
aingsomengly <31507328+aingsomengly@users.noreply.github.com>
2018-06-06 09:02:23 +0700

    update

--- ci_Ecommerce/application/views/inc/location.php
+++
@@ -1,8 +0,0 @@
-	<div class="w3_breadcrumb">
-	<div class="breadcrumb-inner">	
-			<ul>
-				<li><a href="<?php site_url();?>home">Home</a> <i> /</i></li>
-				<li><?php echo $frm_name ;?></li>
-			</ul>
-		</div>
-	</div>
