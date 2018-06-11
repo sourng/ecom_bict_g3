e554da128e3d8ec5e9259057929db92c268ccc9b
Sina Ean <31826916+sinaeean@users.noreply.github.com>
2018-06-11 15:09:45 +0700

    កែប្រែ

--- ci_ecommerce/application/views/inc/footer_styl.php
+++
@@ -1,9 +0,0 @@
-
-    <script src="<?php echo base_url(); ?>public/js/jquery-1.11.0.min.js"></script>
-    <script src="<?php echo base_url(); ?>public/js/bootstrap.min.js"></script>
-    <script src="<?php echo base_url(); ?>public/js/jquery.cookie.js"></script>
-    <script src="<?php echo base_url(); ?>public/js/waypoints.min.js"></script>
-    <script src="<?php echo base_url(); ?>public/js/modernizr.js"></script>
-    <script src="<?php echo base_url(); ?>public/js/bootstrap-hover-dropdown.js"></script>
-    <script src="<?php echo base_url(); ?>public/js/owl.carousel.min.js"></script>
-    <script src="<?php echo base_url(); ?>public/js/front.js"></script>
